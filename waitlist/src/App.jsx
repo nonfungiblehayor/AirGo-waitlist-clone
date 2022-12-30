1a20339c14a7755f067feb1ac866a96362a8ef4e
nonfungiblehayor <nonfungiblehayor.eth@gmail.com>
2022-12-30 13:21:28 +0100

    feat: created banner section

--- waitlist/src/App.jsx
+++ waitlist/src/App.jsx
@@ -3,12 +3,14 @@ import Header from './components/header';
 import SubHeader from './components/sub-header';
 import Work from './components/work';
 import Footer from './components/footer';
+import Section1 from './components/section1';
 
 function App() {
   return (
     <div className={styles.App}>
       <Header /> 
-      <SubHeader />    
+      <SubHeader />   
+      <Section1 /> 
       <Work />
       <Footer />
     </div>
---
+++ waitlist/src/components/section1.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from '../App.module.scss'
+
+function Section1() {
+    return <section className={styles.section1}>
+        <div className={styles.div1}>
+            <h3>Join us in providing easy delivery</h3>
+            <p>
+                Accept a request on the AirGo app to deliver an item and get paid on your next flight!
+            </p>
+        </div>
+        <div className={styles.div2}>
+            
+        </div>
+    </section>
+}
+
+export default Section1
