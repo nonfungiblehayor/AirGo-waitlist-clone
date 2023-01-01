25448bb53b42c23ad48675f82638c4f9ff031b35
nonfungiblehayor <nonfungiblehayor.eth@gmail.com>
2023-01-01 15:17:17 +0100

    style: styled 320-582px responsive view

--- waitlist/src/components/header.jsx
+++ waitlist/src/components/header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import styles from '../App.module.scss'
 import logo from '../images/logo.svg'
+import vector from '../images/Vector.svg'
+// import { useRef } from "react";
 
 function Header() {
     return <header className={styles.header}>
         <a href='/app'> <img src={logo} className={styles.logoImg} alt='airGo logo'></img></a>
+        <img src={vector} className={styles.menu} alt='menu'></img>
         <nav className={styles.navLink}>
             <a href='/app'>Home</a>
             <a href='/app'>How it works</a>
