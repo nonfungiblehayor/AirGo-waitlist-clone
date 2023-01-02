70233afd5f497f721bbbe6163aba29c6d390f759
nonfungiblehayor <nonfungiblehayor.eth@gmail.com>
2023-01-02 13:00:48 +0100

    bug: fix styling bugs

--- waitlist/src/components/header.jsx
+++ waitlist/src/components/header.jsx
@@ -8,14 +8,15 @@ function Header(props) {
     return  <>
        <div className={styles.divLogo}>
         <a href='/app'><img src={logo} alt='airGo' className={styles.logo}></img></a>
-        <img src={vector} alt='menu' onClick={props.fnc}></img>
+        <img src={vector} alt='menu' onClick={props.fnc} className={styles.menu}></img>
        </div>
        <div className={styles.navLink}>
+       <a href='/app'><img src={logo} alt='airGo' className={styles.logo}></img></a>
         <a href='/app'>Home</a>
         <a href='/app'>How it works</a>
         <a href='/app'>Contact us</a>
+        <button className={styles.waitBtn}>Join waitlist</button>
        </div>
-       <button className={styles.waitBtn}>Join waitlist</button>
     </>
 }
 
