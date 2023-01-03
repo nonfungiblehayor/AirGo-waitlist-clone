87d260dc6ce30eb501a612bed568ce9b959c71b8
nonfungiblehayor <nonfungiblehayor.eth@gmail.com>
2023-01-03 15:29:31 +0100

    fix: styling bugs

--- waitlist/src/components/sub-work.jsx
+++ waitlist/src/components/sub-work.jsx
@@ -3,7 +3,9 @@ import styles from '../App.module.scss'
 
 function SubWork(props) {
     return<div className={styles.Subwork}>
+        <div className={styles.divImg}>
         <img src={props.img} alt='description' className={styles.img}></img>
+        </div>
         <h5>{props.upText}</h5>
         <p className={styles.pText}>{props.downText}</p>
     </div>
