1dbf0119ccd8bce3e331eb71ddde80a8a1372d64
nonfungiblehayor <nonfungiblehayor.eth@gmail.com>
2023-01-21 10:27:46 +0100

    feat: implemented section1 component new design

--- waitlist/src/components/section1.jsx
+++ waitlist/src/components/section1.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import styles from '../App.module.scss'
-import frame from '../images/frame.svg'
+import frame from '../images/Frame.svg'
 
 function Section1() {
     return <section className={styles.section1}>
+        <div className={styles.div2}>
+            <img src={frame} alt='frame img'></img>
+        </div>
         <div className={styles.div1}>
             <h3>Join us in providing easy delivery</h3>
             <p>
                 Accept a request on the AirGo app to deliver an item and get paid on your next flight!
             </p> 
+            <button className={styles.waitBtn}>Deliver for us</button>
         </div>  
-        <img src={frame} alt='frame'></img>         
-
     </section>
 }
 
