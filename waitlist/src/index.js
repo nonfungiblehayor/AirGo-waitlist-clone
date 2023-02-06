8210e51ccce7af4f00dbbd0ab9a2b7b9c39adc3a
nonfungiblehayor <nonfungiblehayor.eth@gmail.com>
2023-02-06 14:16:59 +0100

    fix: styling bugs

--- waitlist/src/index.js
+++ waitlist/src/index.js
@@ -8,7 +8,7 @@ import { createBrowserRouter, RouterProvider} from "react-router-dom";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "App",
     element: <App />,
   },
   {
