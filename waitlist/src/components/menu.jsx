bdf8c68633ff8be3ccf69675700fcfb244f5e402
nonfungiblehayor <nonfungiblehayor.eth@gmail.com>
2023-02-06 18:12:55 +0100

    fix: styling bugs

--- waitlist/src/components/menu.jsx
+++ waitlist/src/components/menu.jsx
@@ -4,17 +4,21 @@ import cancel from '../images/cancel.svg'
 import rosebox from '../images/rosebox.svg'
 
 function Menu(props) {
-    return  <nav className={styles.navDiv}>
+    return <>
+    <nav className={styles.navDiv}>
+        <div className={styles.divNav}>
     <img src={cancel} alt='cancel' className={styles.cancel} onClick={props.fnc2}></img>
     <a href='/'>Home</a>
     <a href='#work'>How it works</a>
     <a href='#footer'>Contact us</a>
     <button className={styles.waitBtn2} onClick={props.menuFnc}>Send now</button>
+    </div>
+    </nav>
     <div className={styles.subHead2}>
             <p>Simple, easy deliveries</p>
             <img src={rosebox} alt='delivery'></img>
     </div>
-</nav>
+    </>
 }
 
 export default Menu
